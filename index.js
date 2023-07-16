6d8ab9be5e8b86842e2c94a149ba49eac76f8a8f
MARY NJOKI <marynjoki738@gmail.com>
2023-07-16 11:20:42 +0300

    modifying elements in DOM

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Remove the 'main' element with id 'main'
+var mainElement = document.getElementById('main');
+mainElement.remove();
+
+// Create a new header element
+var newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the new header to the body
+document.body.appendChild(newHeader);
